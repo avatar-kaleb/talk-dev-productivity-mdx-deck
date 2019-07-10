9e7d34c94d7f513520ecfe60f9e2f100e296cf50
Kaleb McKelvey <kmckelvey3@outlook.com>
2019-07-10 08:05:55 -0400

    “cleaning up code and constants”

---
+++ constants.js
@@ -0,0 +1,27 @@
+export const COLORS = {
+  black: 'black',
+  green: 'green',
+  red: 'tomato',
+  white: '#f6f6ff'
+};
+
+export const FONT_WEIGHTS = {
+  regular: 'regular'
+};
+
+export const IMAGE_URLS = {
+  gtd: './images/getting-things-done-diagram.png',
+  intro: './images/intro-slide.png',
+  onenote: './images/one-note-example.png',
+  trello: './images/gtd-trello-board.png'
+};
+
+export const SMALL_BOX_SHADOW = '0 2px 16px rgba(0, 0, 0, 0.25)';
+
+export const VIDEO_URLS = {
+  multiCursorSlow: './videos/ExampleMultiCursorSlow.mp4',
+  multiCursorFast: './videos/ExampleMultiCursorFast.mp4',
+  snippetComponent: './videos/ExampleComponentSnippet.mp4',
+  syncSettings: './videos/ExampleSyncSettingsMixedSpeeds.mp4',
+  typedComponent: './videos/ExampleComponentTyped.mp4'
+};
